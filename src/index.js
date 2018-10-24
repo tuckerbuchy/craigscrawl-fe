7fd588e990ae95e27095dfa72b5bddbd5faead70
Tucker Buchy <tucker.buchy@gmail.com>
2018-10-23 22:16:58 -0700

    react with leaflet kinda going"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import _ from 'lodash';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Map, TileLayer } from 'react-leaflet'
 
-function component() {
-  let element = document.createElement('div');
+const position = [49.282, -123.1207]
+const MyMap = () => {
+  return <Map
+          style={{height: "100vh"}}
+          center={position}
+          zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" />
+        </Map>
+};
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+ReactDOM.render(<MyMap />, document.getElementById("map"));
