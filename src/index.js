a7da4c8c5695e164357e78754eed6c2590c2d505
Tucker Buchy <tucker.buchy@gmail.com>
2018-11-27 22:24:43 -0800

    removed react

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom";
-import { Map, TileLayer, GeoJSON, L } from 'react-leaflet'
-
+import L from 'leaflet';
+import './styles/map.css';
 import vancouver from './vancouver'
 
-const position = [49.26, -123.1207]
-export default class MyMap extends Component {
-	clickFeature(event) {
-		const layer = event.target;
-		layer.setStyle({color: 'green'});
-	}
-	onEachFeature(feature, layer) {
-	  	console.log(layer);
-	  	layer.setStyle({color: 'purple'});
-	    
-	  	layer.on({
-	    	click: this.clickFeature
-	    });
-	  };
-	render() {
-	  return <Map
-	  		  touchZoom={false}
-	  		  minZoom={12}
-	  		  maxZoom={12}
-	  		  zoomControl={false}
-	  		  scrollWheelZoom={false}
-	          style={{height: "100vh"}}
-	          center={position}
-	          zoom={12} >
-	          <TileLayer
-		        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-	            attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors" />
-	          <GeoJSON 
-	            key={Math.random()}
-	          	data={vancouver} 
-	          	color='red'
-                onEachFeature={this.onEachFeature.bind(this)}
-	          	/>
-	        </Map>
-	};
+// GLOBALS
+// TODO: Probably bad, its a global.
+var geoJsonVan = null;
+const ZOOM = 12;
 
+function clickFeature(event) {
+  // Reset the style back to 'unselected'.
+  geoJsonVan.eachLayer((layer) => {
+    layer.setStyle({color: 'purple'});
+  });
+
+  const layer = event.target;
+  layer.setStyle({color: 'green'});
 }
 
+function onEachFeature(feature, layer) {
+  layer.setStyle({color: 'purple'});
+  layer.bindPopup('<h1>'+feature.properties.name+'</h1>');
+  
+  layer.on({
+    click: clickFeature
+  });
+};
 
-ReactDOM.render(<MyMap />, document.getElementById("map"));
+function configureMap() {
+  const map = L.map('mapid', {
+    zoomControl: false,
+    zoom: ZOOM, 
+    minZoom: ZOOM, 
+    maxZoom: ZOOM,
+    scrollWheelZoom: false,
+    touchZoom: false,
+
+  }).setView([49.26, -123.1207], ZOOM);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  geoJsonVan = L.geoJSON(vancouver,  {
+      onEachFeature: onEachFeature
+  });
+
+  geoJsonVan.addTo(map);
+}
+
+configureMap();
+
