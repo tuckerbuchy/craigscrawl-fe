a57282ffc5bf466786632e3d74faa52ed80b9745
Tucker Buchy <tucker.buchy@gmail.com>
2019-03-16 23:30:48 -0700

    got real listings from the api displaying

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import L from 'leaflet';
+import * as heat from 'leaflet.heat'
+import * as d3 from "d3";
 import './styles/map.css';
 import vancouver from './vancouver';
-const { getListings } = require('./services/listings');
+
+const { getListings, getListingsMeanRent } = require('./services/listings');
 
 // GLOBALS
 // TODO: Probably bad, its a global.
@@ -18,9 +21,13 @@ function clickFeature(event) {
   layer.setStyle({color: 'green'});
 }
 
-function onEachFeature(feature, layer) {
+async function onEachFeature(feature, layer) {
+  const neighborhoodName = feature.properties.name;
+
+  const rent = await getListingsMeanRent(neighborhoodName);
+
   layer.setStyle({color: 'purple'});
-  layer.bindPopup('<h1>'+feature.properties.name+'</h1>');
+  layer.bindPopup(`<h1> ${feature.properties.name} </h1> avg rent: ${rent}`);
   
   layer.on({
     click: clickFeature
@@ -33,19 +40,21 @@ function addListingMarkers(listings, map) {
     const lon = l['lon'];
     if (lat && lon){
       const marker = L.marker([lat, lon]).addTo(map); 
+      marker.bindPopup(
+        `<b>${l['title']}</b><br> neighborhood: ${l['neighborhood']}`
+      ).openPopup();
     }
   });
 }
 
 function configureMap(listings) {
-
   const map = L.map('mapid', {
-    zoomControl: false,
+    zoomControl: true,
     zoom: ZOOM, 
     minZoom: ZOOM, 
-    maxZoom: ZOOM,
-    scrollWheelZoom: false,
-    touchZoom: false,
+    // maxZoom: ZOOM,
+    scrollWheelZoom: true,
+    touchZoom: true,
 
   }).setView([49.26, -123.1207], ZOOM);
 
@@ -53,18 +62,35 @@ function configureMap(listings) {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  geoJsonVan = L.geoJSON(vancouver,  {
-      onEachFeature: onEachFeature
-  });
-
-  geoJsonVan.addTo(map);
-  
+  // getHeatLayer(listings).addTo(map);
+  getGeoJsonLayer().addTo(map);
   addListingMarkers(listings, map)
 
   return map;
 }
 
-const listings = getListings();
-const listingsSub = listings.slice(0, 100);
-const map = configureMap(listingsSub);
+function getHeatLayer(listings) {
+  let filteredListings = removeListingOutliers(listings);
+  let maxPrice = 0;
+  let heatMapListings = filteredListings.map( (l) => {
+    let price = l['price'];
+    if (price > maxPrice) maxPrice = price;
+    return [l['lat'], l['lon'], price]
+  });
+  return L.heatLayer(heatMapListings, {blur: 15, max: maxPrice});
+}
 
+function getGeoJsonLayer() {
+  return L.geoJSON(vancouver,  {
+      onEachFeature: onEachFeature
+  });
+}
+
+function removeListingOutliers(listings) {
+  let sortedListings = listings.sort(function(l1, l2){return l1['price'] - l2['price']});
+  let low = Math.round(sortedListings.length * 0.025);
+  let high = sortedListings.length - low;
+  return sortedListings.slice(low,high);
+}
+
+getListings().then(configureMap);
